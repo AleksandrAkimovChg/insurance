c0ca764097d4c24503544ba6cfcb2b52df09cf42
Aleksandr Akimov <gtwood@yandex.ru>
2024-11-11 21:31:21 +0300

    удалил неиспользуемые импорты

--- src/main/java/com/javaacademy/insurance/util/Generator.java
+++ src/main/java/com/javaacademy/insurance/util/Generator.java
@@ -1,8 +1,5 @@
 package com.javaacademy.insurance.util;
 
-import com.javaacademy.insurance.contract.Country;
-import org.springframework.beans.factory.annotation.Value;
-
 public class Generator {
     private static long count = 1;
 
